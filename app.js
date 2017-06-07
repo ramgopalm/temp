e6222615c04dec268107d1a78b55b5ebb3a665c3
Ramgopal Mangena <ramgopal.mangena@gmail.com>
2017-06-07 10:15:45 -0500

    changes to test codedeploy and ec2

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
+var express = require("express");
+var app = express();
 
-var http = require("http");
 
-http.createServer(function (request, response) {
+app.get("/",function(req,res){
+        res.send("<h1>Hello from EC2</h1>");
+});
 
-   // Send the HTTP header
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-
-   // Send the response body as "Hello World"
-   response.end('Hello World2\n');
-}).listen(80);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:80/');
-
+app.listen(80);
